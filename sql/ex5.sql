31c289ecd0661e11a8a40fb554e5f8b2280809e6
Miguel B. Henriques <103072223+MiguelHenri@users.noreply.github.com>
2024-05-20 11:41:34 -0300

    igualando sql 5

--- sql/ex5.sql
+++ sql/ex5.sql
@@ -1,7 +1,27 @@
 SELECT * FROM Propriedades;
 
-SELECT CasaInteira, COUNT(*) FROM Propriedades
+SELECT CasaInteira, COUNT(*) AS Quantidade
+FROM Propriedades
 GROUP BY CasaInteira;
+
+SELECT NQuartos, COUNT(*) AS Quantidade 
+FROM Propriedades 
+GROUP BY NQuartos;
+
+SELECT NBanheiros, COUNT(*) AS Quantidade 
+FROM Propriedades 
+GROUP BY NBanheiros;
+
+SELECT
+    CASE
+        WHEN PrecoDiaria BETWEEN 0 AND 200 THEN 'Classe C'
+        WHEN PrecoDiaria BETWEEN 200 AND 400 THEN 'Classe B'
+        ELSE 'Classe A'
+    END AS ClassePreco,
+    COUNT(*) AS NumeroPropriedades
+FROM Propriedades P
+GROUP BY ClassePreco
+ORDER BY ClassePreco;
 
 SELECT L.Cidade, COUNT(*) FROM Propriedades P
 JOIN Localizacoes L ON P.IDLocalizacao = L.ID
