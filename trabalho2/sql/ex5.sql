c3f316a254f2c9b4abb81610d377cb1219a69869
Albert <albshoji@gmail.com>
2024-06-29 19:30:31 -0300

    terminando ajustes

--- trabalho2/sql/ex5.sql
+++ trabalho2/sql/ex5.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS Propriedades CASCADE;
 CREATE TABLE Propriedades(
-    ID BIGINT NOT NULL, 
-    Nome VARCHAR(50) NOT NULL,
-    Endereco VARCHAR(100) NOT NULL,
-    IDLocalizacao INT NOT NULL,
-    IDAnfitriao INT NOT NULL,
-    NQuartos INT NOT NULL,
-    NBanheiros INT NOT NULL,
-    PrecoDiaria float NOT NULL,
-    NMaxHospedes INT NOT NULL,
-    MinNoites INT NOT NULL,
-    MaxNoites INT NOT NULL,
+    ID BIGINT , 
+    Nome VARCHAR(300) ,
+    Endereco VARCHAR(100) ,
+    IDLocalizacao INT ,
+    IDAnfitriao INT ,
+    NQuartos INT ,
+    NBanheiros INT ,
+    PrecoDiaria float ,
+    NMaxHospedes INT ,
+    MinNoites INT ,
+    MaxNoites INT ,
     ValorLimpeza FLOAT,
     HoraCheckIn TIME,
     HoraCheckOut TIME,
@@ -20,35 +20,35 @@ CREATE TABLE Propriedades(
 
 DROP TABLE IF EXISTS Localizacoes CASCADE; 
 CREATE TABLE Localizacoes(
-    ID INT NOT NULL,
-    Pais VARCHAR(50) NOT NULL,
-    Estado VARCHAR(50) NOT NULL,
-    Cidade VARCHAR(50) NOT NULL,
-    Bairro VARCHAR(50) NOT NULL
+    ID INT ,
+    Pais VARCHAR(50) ,
+    Estado VARCHAR(50) ,
+    Cidade VARCHAR(50) ,
+    Bairro VARCHAR(50) 
 );
 
 DROP TABLE IF EXISTS Usuarios CASCADE;
 CREATE TABLE Usuarios(
-    ID INT NOT NULL,
-    Nome VARCHAR(50) NOT NULL,
-    Email VARCHAR(50) NOT NULL,
-    Sobrenome VARCHAR(50) NOT NULL,
-    Nascimento DATE NOT NULL,
-    Endereco VARCHAR(100) NOT NULL,
-    Sexo VARCHAR(50) NOT NULL,
-    Telefone VARCHAR(50) NOT NULL,
-    Senha VARCHAR(50) NOT NULL,
-    IDLocalizacao INT NOT NULL,
-    EAnfritriao VARCHAR(50)
+    ID INT ,
+    Nome VARCHAR(100) ,
+    Email VARCHAR(50) ,
+    Sobrenome VARCHAR(100) ,
+    Nascimento DATE ,
+    Endereco VARCHAR(100) ,
+    Sexo VARCHAR(50) ,
+    Telefone VARCHAR(50) ,
+    Senha VARCHAR(50) ,
+    IDLocalizacao INT ,
+    EAnfitriao BOOLEAN
 );
 
 DROP TABLE IF EXISTS Mensagens CASCADE;
 CREATE TABLE Mensagens(
-    IDPropriedade BIGINT NOT NULL,
-    DataCriacao VARCHAR (50) NOT NULL,
-    IDRemetente INT NOT NULL,
-    IDDestinatario INT NOT NULL,
-    Texto VARCHAR(200) NOT NULL,
+    IDPropriedade BIGINT ,
+    DataCriacao VARCHAR (50) ,
+    IDRemetente INT ,
+    IDDestinatario INT ,
+    Texto VARCHAR(200) ,
     ClassificacaoLocalizacao VARCHAR(50),
     ClassificacaoComunicacao VARCHAR(50),
     ClassificacaoValor VARCHAR(50),
@@ -57,43 +57,43 @@ CREATE TABLE Mensagens(
 
 DROP TABLE IF EXISTS Locacao CASCADE;
 CREATE TABLE Locacao(
-    IDHospede INT NOT NULL,
-    IDPropriedade BIGINT NOT NULL,
-    DataReserva DATE NOT NULL,
-    DataCheckIn DATE NOT NULL,
-    DataCheckOut DATE NOT NULL,
-    NHospedes INT NOT NULL,
+    IDHospede INT ,
+    IDPropriedade BIGINT ,
+    DataReserva DATE ,
+    DataCheckIn DATE ,
+    DataCheckOut DATE ,
+    NHospedes INT ,
     PrecoTotal FLOAT,
     Imposto FLOAT,
     PrecoImpostos FLOAT,
     CodigoPromocional VARCHAR(50),
     ValorDesconto FLOAT,
-    Confirmada BOOLEAN NOT NULL
+    Confirmada BOOLEAN 
 );
 
 
 DROP TABLE IF EXISTS PontosInteresse CASCADE;
 CREATE TABLE PontosInteresse(
-    IDLocalizacao INT NOT NULL,
-    NOME VARCHAR(50) NOT NULL
+    IDLocalizacao INT ,
+    NOME VARCHAR(50) 
 );
 
 
 DROP TABLE IF EXISTS Fotos CASCADE;
 CREATE TABLE Fotos(
     DataCriacao DATE,
-    Foto BYTEA NOT NULL,
+    Foto BYTEA ,
     IDRemetente INT,
     IDDestinatario INT
 );
 
 DROP TABLE IF EXISTS Quartos CASCADE;
 CREATE TABLE Quartos(
-    ID SERIAL NOT NULL,
-    NSolteiro INT NOT NULL,
-    NCasal INT NOT NULL,
-    Individual BOOLEAN NOT NULL,
-    IDPropriedade BIGINT NOT NULL
+    ID SERIAL ,
+    NSolteiro INT ,
+    NCasal INT ,
+    Individual BOOLEAN ,
+    IDPropriedade BIGINT 
 );
 
 DROP TABLE IF EXISTS Comodidades CASCADE;
@@ -161,23 +161,26 @@ CREATE TABLE Calendar(
     maximum_nights INT
 );
 
-INSERT INTO Mensagens (DataCriacao) --preencher as tabelas com os dados do reviews
-SELECT date_
-FROM Reviews;
-
-INSERT INTO Propriedades (ID) --preencher as tabelas com os dados do reviews
-SELECT listing_id
+INSERT INTO Mensagens (IDPropriedade, DataCriacao) --preencher as tabelas com os dados do reviews
+SELECT listing_id, date_
 FROM Reviews;
 
 INSERT INTO Propriedades (ID, MinNoites, MaxNoites) --preencher as tabelas com os dados do calendar
 SELECT listing_id, minimum_nights, maximum_nights
 FROM Calendar;
 
-INSERT INTO Locacao (DataReserva, PrecoTotal, PrecoImpostos) 
-SELECT date_, price, adjusted_price
+INSERT INTO Locacao (IDPropriedade, DataReserva, PrecoImpostos, PrecoTotal)
+SELECT listing_id, 
+       date_, 
+       CAST(REPLACE(REPLACE(adjusted_price, '$', ''), ',', '') AS FLOAT), 
+       CAST(REPLACE(REPLACE(price, '$', ''), ',', '') AS FLOAT)
 FROM Calendar;
 
 INSERT INTO Propriedades (ID, Nome, IDAnfitriao, PrecoDiaria, MinNoites) --preencher as tabelas com os dados do listings
 SELECT ID, name_, host_id, price, minimum_nights --n sei colocar o nome do anfitriao
+FROM Listings;
+
+INSERT INTO Usuarios (ID, Nome, EAnfitriao)
+SELECT host_id, host_name, TRUE
 FROM Listings;
 
